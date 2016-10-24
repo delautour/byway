af1642e894dfb285b92cfd77f3067e79a5741615
Courtney de Lautour <courtney.de.lautour@gmail.com>
2016-10-24 21:40:45 +0800

    Service?

--- byway-proxy/byway-proxy_windows.go
+++ byway-proxy/byway-proxy_windows.go
@@ -6,10 +6,37 @@ import (
 	"github.com/amerdrix/byway/config"
 	"github.com/amerdrix/byway/core"
 	"golang.org/x/sys/windows/svc"
+	"golang.org/x/sys/windows/svc/debug"
 )
+
+type bywayService struct{}
+
+func (m *bywayService) Execute(args []string, r <-chan svc.ChangeRequest, changes chan<- svc.Status) (ssec bool, errno uint32) {
+	const cmdsAccepted = svc.AcceptStop | svc.AcceptShutdown
+	changes <- svc.Status{State: svc.StartPending}
+	changes <- svc.Status{State: svc.Running, Accepts: cmdsAccepted}
+
+loop:
+	for {
+		c := <-r
+		switch c.Cmd {
+		case svc.Stop, svc.Shutdown:
+			process.Exit()
+			break loop
+		default:
+			debug.Log.Error(1, fmt.Sprintf("unexpected control request #%d", c))
+		}
+	}
+
+	changes <- svc.Status{State: svc.StopPending}
+	return
+}
 
 func main() {
 	interactive, _ := svc.IsAnInteractiveSession()
+	if !interactive {
+		svc.Run("Byway", &bywayService{})
+	}
 
 	fmt.Printf("Welcome to byway - windows %b!", interactive)
 
